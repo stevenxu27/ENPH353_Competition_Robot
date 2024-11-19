d018c5e77cac5f79be7d62b185e9f1c82d3eabdd
stevenxu27 <steven.x2085@gmail.com>
2024-11-19 01:30:11 -0800

    Time trials code

---
+++ src/controller_node.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import rospy
+import time
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+def main():
+    # Initialize the ROS node
+    rospy.init_node('controller_node', anonymous=True)
+
+    time.sleep(1)
+
+    # Create a publisher to send velocity commands
+    image_pub = rospy.Publisher('/B1/cmd_vel', Twist, queue_size=1)
+    score_pub = rospy.Publisher('/score_tracker', String)
+
+    rospy.sleep(1)
+
+    # Create a rate object to control the publishing rate (30 Hz)
+    rate = rospy.Rate(30)  # 30 Hz loop rate
+
+    team_name = "TeamX"
+    password = "password123"
+
+    start_message = f"{team_name},{password},0,NAa"
+    rospy.loginfo(f"Publishing start message: {start_message}")
+    score_pub.publish(start_message)
+
+    move = Twist()
+    move.linear.x = 0.25
+    move.angular.z = 0.1
+    image_pub.publish(move)
+    rate.sleep()
+
+    rospy.sleep(5)
+
+    move.linear.x = 0
+    move.angular.z = 0
+    image_pub.publish(move)
+
+    stop_message = f"{team_name},{password},-1,NAa"
+    rospy.loginfo(f"Publishing stop message: {stop_message}")
+    score_pub.publish(stop_message)
+    rate.sleep()
+
+    rospy.sleep(1)
+
+    # while not rospy.is_shutdown():
+    #     # Publish the move command
+    #     image_pub.publish(move)
+
+    #     # Sleep to maintain the rate
+    #     rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Shutting down.")
+
