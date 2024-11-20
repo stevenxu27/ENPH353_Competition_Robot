873cbf2c301e6ad4399fbcfc14a1b284cddfea7f
stevenxu27 <steven.x2085@gmail.com>
2024-11-20 00:31:56 -0800

    Updated team name and longer drive duration for time trials

--- src/controller_node.py
+++ src/controller_node.py
@@ -5,13 +5,14 @@ import time
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
+
 def main():
     # Initialize the ROS node
     rospy.init_node('controller_node', anonymous=True)
-
+    # sleep after initialization
     time.sleep(1)
 
-    # Create a publisher to send velocity commands
+    # Create a publisher to send velocity commands and score tracker
     image_pub = rospy.Publisher('/B1/cmd_vel', Twist, queue_size=1)
     score_pub = rospy.Publisher('/score_tracker', String)
 
@@ -20,10 +21,10 @@ def main():
     # Create a rate object to control the publishing rate (30 Hz)
     rate = rospy.Rate(30)  # 30 Hz loop rate
 
-    team_name = "TeamX"
-    password = "password123"
+    team_name = "The McDoubles"
+    password = "password"
 
-    start_message = f"{team_name},{password},0,NAa"
+    start_message = f"{team_name},{password},0, Starting Simulation: Moving Forward"
     rospy.loginfo(f"Publishing start message: {start_message}")
     score_pub.publish(start_message)
 
@@ -33,13 +34,13 @@ def main():
     image_pub.publish(move)
     rate.sleep()
 
-    rospy.sleep(5)
+    rospy.sleep(20)
 
     move.linear.x = 0
     move.angular.z = 0
     image_pub.publish(move)
 
-    stop_message = f"{team_name},{password},-1,NAa"
+    stop_message = f"{team_name},{password},-1,Finishing Simulation: Stop"
     rospy.loginfo(f"Publishing stop message: {stop_message}")
     score_pub.publish(stop_message)
     rate.sleep()
