644c18a77e1abd1f3aaa74f45b821bb20de5a63a
stevenxu27 <steven.x2085@gmail.com>
2024-12-03 04:03:02 -0800

    Up to date model for image recognition

--- scripts/image_processing.py
+++ scripts/image_processing.py
@@ -42,14 +42,14 @@ def predict_letter(roi):
 def slice_image(image):
     # Slice the image into 4 sections (adjust these coordinates as needed)
     first_character = image[250:350, 25:75]
-    second_character = image[250:350, 85:135]
-    third_character = image[250:350, 140:190]
-    fourth_character = image[250:350, 200:250]
-    fifth_character = image[250:350, 250:300]
-    sixth_character = image[250:350, 300:350]
-    seventh_character = image[250:350, 350:400]
-    eigth_character = image[250:350, 410:460]
-    ninth_character = image[250:350, 460:510]
+    second_character = image[250:350, 75:125]
+    third_character = image[250:350, 120:170]
+    fourth_character = image[250:350, 170:220]
+    fifth_character = image[250:350, 215:265]
+    sixth_character = image[250:350, 265:315]
+    seventh_character = image[250:350, 320:370]
+    eigth_character = image[250:350, 375:425]
+    ninth_character = image[250:350, 425:475]
 
     return first_character, second_character, third_character, fourth_character, fifth_character, sixth_character, seventh_character, eigth_character, ninth_character
 
@@ -70,7 +70,7 @@ def image_callback(msg):
         first, second, third, fourth, fifth, sixth, seventh, eighth, ninth = slice_image(cv_image)
 
         message = ''
-        for i, character in enumerate([first, second, third, fourth, fifth]):
+        for i, character in enumerate([first, second, third, fourth, fifth, sixth]):
             rospy.loginfo(f"OpenCV image shape: {character.shape}")
             cv2.imshow('Test character', character)
             predicted_class = predict_letter(character)
