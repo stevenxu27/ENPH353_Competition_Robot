da73812b2c3e51ad54a8a757762783e1ce748a76
stevenxu27 <steven.x2085@gmail.com>
2024-12-03 22:43:20 -0800

    Best Working Model for now

--- scripts/image_processing.py
+++ scripts/image_processing.py
@@ -8,10 +8,9 @@ import cv2
 import time
 import numpy as np
 from std_msgs.msg import String
-
 from tensorflow.keras.models import load_model
 
-model_path = "/home/fizzer/ros_ws/src/controller/models/test_model.h5"
+model_path = "/home/fizzer/ros_ws/src/controller/models/working_model.h5"
 model = load_model(model_path)
 
 # Define the image size you want to slice
@@ -23,22 +22,28 @@ classes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 '  # String of values
 class_to_index = {char: idx for idx, char in enumerate(classes)}  # Map char -> index
 index_to_class = {idx: char for char, idx in class_to_index.items()}  # Map index -> char
 
-counter = 1
+clue_map = {
+    'SIZE': '1',
+    'VICTIM': '2',
+    'CRIME': '3',
+    'TIME': '4',
+    'PLACE': '5',
+    'MOTIVE': '6',
+    'WEAPON': '7',
+    'BANDIT': '8',
+}
+
+
+score_pub = rospy.Publisher('/score_tracker', String)
 
 def preprocess_image(img):
-    # Resize to the size the model expects (100, 50, 3)
-    # img_resized = cv2.resize(img, (slice_width, slice_height))
-    # img_normalized = img_resized / 255.0  # Normalize to [0, 1]
     img_expanded = np.expand_dims(img, axis=0)  # Add batch dimension
     return img_expanded
 
 def predict_letter(roi):
-    
-    # Predict using the model
+
     roi_preprocessed = preprocess_image(roi)
     prediction = model.predict(roi_preprocessed)
-    
-    # Get the predicted class (for classification models)
     predicted_class = np.argmax(prediction)
     
     return predicted_class
@@ -59,6 +64,25 @@ def slice_image(image):
     twelvth_character = image[250:350, 525:570]
     return first_character, second_character, third_character, fourth_character, fifth_character, sixth_character, seventh_character, eigth_character, ninth_character, tenth_character, eleventh_character, twelvth_character
 
+def slice_clue(image):
+    first_character = image[30:130, 250:295]
+    second_character = image[30:130, 295:340]
+    third_character = image[30:130, 340:385]
+    fourth_character = image[30:130, 385:430]
+    fifth_character = image[30:130, 430:475]
+    sixth_character = image[30:130, 475:520]
+    return first_character, second_character, third_character, fourth_character, fifth_character, sixth_character
+
+def find_clue(message):
+
+    return clue_map.get(message, '100')
+
+def is_between_0_and_6(s):
+    if s.isdigit():  # Check if the string is numeric
+        number = int(s)  # Convert to an integer
+        return 0 <= number <= 6  # Check if it's in the range
+    return False  # Return False if not numeric
+
 def image_callback(msg):
 
     global counter  # Declare counter as global to modify it
@@ -76,26 +100,30 @@ def image_callback(msg):
         # cv2.imshow("Received Homography", cv_image[250:350, 25:75])
 
         first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, eleventh, twelvth = slice_image(cv_image)
+        first_character, second_character, third_character, fourth_character, fifth_character, sixth_character = slice_clue(cv_image)
 
-        score_pub = rospy.Publisher('/score_tracker', String)
+        clue = ''
+        for i, character in enumerate([first_character, second_character, third_character, fourth_character, fifth_character, sixth_character]):
+            cv2.imshow('Test character', character)
+            predicted_class = predict_letter(character)
+            clue += str(index_to_class.get(predicted_class))
 
         message = ''
         for i, character in enumerate([first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, eleventh, twelvth]):
-            rospy.loginfo(f"OpenCV image shape: {character.shape}")
-            cv2.imshow('Test character', character)
+            # cv2.imshow('Test character', character)
             predicted_class = predict_letter(character)
             message += str(index_to_class.get(predicted_class))
-            # rospy.loginfo(f"Predicted class for character {i+1}: {predicted_class}")
 
         team_name = "The McDoubles"
-        password = "password"
+        password = "Junior Chicken"
 
-
-        start_message = f"{team_name},{password},{counter}, {message}"
-        rospy.loginfo(f"Publishing start message: {start_message}")
-        score_pub.publish(start_message)
-
-        counter += 1
+        clue = clue.replace(" ", "")
+        
+        if (is_between_0_and_6(find_clue(clue))):
+            start_message = f"{team_name},{password},{find_clue(clue)}, {message}"
+            score_pub.publish(start_message)
+        rospy.loginfo(f"Clue: {clue}")
+        rospy.loginfo(f"message: {message}")
 
         rospy.loginfo(f"Predicted message: {message}")
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -109,8 +137,6 @@ def main():
     rospy.init_node('image_processing', anonymous=True)
 
     time.sleep(1)
-
-    score_pub = rospy.Publisher('/score_tracker', String)
 
     team_name = "The McDoubles"
     password = "password"
